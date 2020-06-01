317197b422f762e528eae0d2f9e1bf650aac66bf
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-06-01 21:50:10 +0100

    Variable mutation and type coercion

---
+++ JS_Basics/script.js
@@ -0,0 +1,25 @@
+// Variable Mutation and Type Coercion
+
+var firstName = 'John';
+var age = 28;
+
+// type coercion => when JS automatically converts types from one another as its needed, eg. firstName is a string, space is a string and it converts age from a number to a string
+console.log(firstName + ' ' + age);
+
+var job, isMarried; //this is going to be undefined
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation => to change the value of a variable
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?'); // prompt asks for user's input
+
+console.log(firstName + ' ' + lastName);
+
+
