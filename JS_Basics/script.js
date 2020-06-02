4cf594eb8c08622951eb71c093d5f9fc2fd43b7d
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-06-02 17:29:01 +0100

    Boolean logic

--- JS_Basics/script.js
+++ JS_Basics/script.js
@@ -92,18 +92,35 @@
  */
 
 
-var firstName = 'John';
-var civilStatus = 'single';
+// var firstName = 'John';
+// var civilStatus = 'single';
 
-if (civilStatus === 'married') {
-  console.log(firstName + " is married!");
-} else {
-  console.log(firstName + " is single!");
-}
+// if (civilStatus === 'married') {
+//   console.log(firstName + " is married!");
+// } else {
+//   console.log(firstName + " is single!");
+// }
 
-var isMarried = true;
-if (isMarried) {
-  console.log(firstName + " is married!");
+// var isMarried = true;
+// if (isMarried) {
+//   console.log(firstName + " is married!");
+// } else {
+//   console.log(firstName + " is single!");
+// }
+
+
+
+/*********************
+ * Boolean logic
+ */
+
+var firstName = "John";
+var age = 16;
+
+if (age < 13) {
+  console.log(firstName + " is a boy");
+} else if (age >= 13 && age < 20) { // between 13 and 20 => age >= 13 AND age < 20
+  console.log(firstName + " is a teenager");
 } else {
-  console.log(firstName + " is single!");
+  console.log(firstName + " is a man");
 }
