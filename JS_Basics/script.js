829b155ddd743c709535bdb4133ce08710c68fef
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-06-02 16:56:53 +0100

    If/else statements

--- JS_Basics/script.js
+++ JS_Basics/script.js
@@ -84,5 +84,26 @@
 
 
 /****************
- * Coding challenge 1
+ * Coding challenge 1 - In a separare folder
  */
+
+/****************
+ * If / else statements
+ */
+
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+  console.log(firstName + " is married!");
+} else {
+  console.log(firstName + " is single!");
+}
+
+var isMarried = true;
+if (isMarried) {
+  console.log(firstName + " is married!");
+} else {
+  console.log(firstName + " is single!");
+}
