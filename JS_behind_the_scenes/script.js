956e54aba2652aea9fdb3aaf303377c0f0f794df
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-06-18 15:37:51 +0100

    Hoisting and execution context objects

--- JS_behind_the_scenes/script.js
+++ JS_behind_the_scenes/script.js
@@ -1,31 +1,36 @@
 /////////////////////////////////////////
-// Lecture: Hoisting
+// Lecture: Hoisting => functions and variables are hoisted in JS, which means that they are avilable before the the execution phase starts. The functions are already defined before the execution phase starts, while variables are set up to undefined and will only be defined in the execution phase.
 
 //functions
+
+// up to now we have first declared the function and then call it
 calculateAge(1965);
 
 function calculateAge(year) {
   console.log(2016 - year);
 }
+// calculateAge(1990);
 
-// retirement(1990);
+
+// retirement(1965); => this will not work because it's not a function declaration but a function expression and hoisting with functions works only for function declarations
 var retirement = function(year) {
   console.log(65 - (2016 - year));
 }
 
 //variables
-console.log(age);
+console.log(age); // => won't work and will come up as undefined.In the creation phase of the variable object what happens is that the code is scanned for variable declarationsand the variables are then set to undefined. Js knows that there will be av age variable but we don't have the value yet.
 var age = 23;
-// console.log(age);
+// console.log(age); => works
 
 
 function foo() {
+  // console.log(age); => this will come back as undefined because of the hoisting
   var age = 65;
   console.log(age);
 }
 
-foo();
-console.log(age);
+foo(); // prints the age of 65 => gets its own execution context object(the foo function) in which we can store a variable and it can be the same name
+console.log(age); // prints the age of 23 => this variable gets stored in the global execution context object
 
 
 
