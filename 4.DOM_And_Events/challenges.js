18f3a9c18a84df3e76276285dee173dcd1e38010
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-08-05 11:43:54 +0100

    Solution to challenge 6 part 2

--- 4.DOM_And_Events/challenges.js
+++ 4.DOM_And_Events/challenges.js
@@ -52,9 +52,17 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // 2. Update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     var input = document.querySelector('.final-score').value;
+    var winningScore;
     
+    // We want to check what happens in case the final score is empty. All values of undefined, 0, null or "" are COERCED to false. Anything else is COERCED to true
+    if(input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
     // 3. Check if player won the game 
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= winningScore) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
