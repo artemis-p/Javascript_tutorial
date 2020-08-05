d209341703a50cac67014e128518f58e98c554db
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-08-05 18:04:28 +0100

    The function constructor

---
+++ 5.Advanced_JS/script.js
@@ -0,0 +1,23 @@
+// Function constructor (always written with a capital letter)
+var john = {
+  name: 'John',
+  yearOfBirth: 1990,
+  job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+  console.log(2016 - this.yearOfBirth)
+};
+
+var john = new Person("John", 1990, "teacher"); // instantiation 
+var jane = new Person("Jane", 1988, "doctor"); 
+var mark = new Person("Mark", 1960, "dentist"); 
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
