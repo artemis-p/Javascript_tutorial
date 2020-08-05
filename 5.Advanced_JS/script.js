3ef7e7be99300ef6710e1702e4d8b85c96f69258
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-08-05 18:08:24 +0100

    Added property with prototype

--- 5.Advanced_JS/script.js
+++ 5.Advanced_JS/script.js
@@ -15,9 +15,15 @@ Person.prototype.calculateAge = function() {
   console.log(2016 - this.yearOfBirth)
 };
 
+Person.prototype.lastName = "Smith";
+
 var john = new Person("John", 1990, "teacher"); // instantiation 
 var jane = new Person("Jane", 1988, "doctor"); 
 var mark = new Person("Mark", 1960, "dentist"); 
 john.calculateAge();
 jane.calculateAge();
 mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
