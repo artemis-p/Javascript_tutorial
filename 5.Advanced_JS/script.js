ce63bc1cb5ac2cc7ed82d99f9791049e81614d5d
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-08-06 14:56:32 +0100

    Functions returning functions

--- 5.Advanced_JS/script.js
+++ 5.Advanced_JS/script.js
@@ -165,34 +165,64 @@
 
 
 // Example 4:
-var years = [1990, 1965, 1937, 2005, 1998];
-function arrayCalc(arr, fn) {
-  var arrRes = [];
-  for (var i = 0; i < arr.length; i++) {
-    arrRes.push(fn(arr[i]));
-  }
-  return arrRes;
-}
+// var years = [1990, 1965, 1937, 2005, 1998];
+// function arrayCalc(arr, fn) {
+//   var arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+//   return arrRes;
+// }
 
-function calculateAge(el) {
-  return 2016 - el;
-}
+// function calculateAge(el) {
+//   return 2016 - el;
+// }
 
-function isFullAge(el) {
-  return el >= 18;
-}
+// function isFullAge(el) {
+//   return el >= 18;
+// }
 
-function maxHeartRate(el) {
-  if (el >= 18 && el <= 81) {
-    return Math.round(206.9 - (0.67 * el));
+// function maxHeartRate(el) {
+//   if (el >= 18 && el <= 81) {
+//     return Math.round(206.9 - (0.67 * el));
+//   } else {
+//     return -1;
+//   }
+// }
+
+// var ages = arrayCalc(years, calculateAge); // calculateAge is a callback function cause we are not calling it. we want the arrayCalc function to later call the calculateAge function
+// var fullAges = arrayCalc(ages, isFullAge);
+// var rates = arrayCalc(ages, maxHeartRate);
+// console.log(ages);
+// console.log(fullAges);
+// console.log(rates);
+
+
+////////////
+// Functions returning functions
+
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) { // anonymous function
+      console.log(name + ', can you please explain what UX design is?');
+    } 
+  } else if (job === 'teacher') {
+      return function(name) {
+        console.log('What subject do you teach, ' + name + '?');
+    }
   } else {
-    return -1;
-  }
+      return function(name) {
+        console.log('Hello ' + name + ', what do you do?');
+      };
+    };
 }
 
-var ages = arrayCalc(years, calculateAge); // calculateAge is a callback function cause we are not calling it. we want the arrayCalc function to later call the calculateAge function
-var fullAges = arrayCalc(ages, isFullAge);
-var rates = arrayCalc(ages, maxHeartRate);
-console.log(ages);
-console.log(fullAges);
-console.log(rates);
+var teacherQuestion = interviewQuestion('teacher');
+teacherQuestion('John');
+var designerQuestion = interviewQuestion('designer');
+designerQuestion('Mark');
+var unkownQuestion = interviewQuestion('chef');
+unkownQuestion('Luna');
+
+//alternatively: 
+interviewQuestion('teacher')('John');
