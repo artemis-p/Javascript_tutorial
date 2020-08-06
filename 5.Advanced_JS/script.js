9c8edd40d473427ae3ec5803e9ec78563e5117ec
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-08-06 11:54:02 +0100

    Callback functions and how to pass functions as arguments

--- 5.Advanced_JS/script.js
+++ 5.Advanced_JS/script.js
@@ -83,17 +83,116 @@
 
 //passing a primitive and object into a function - functions
 
-var age = 27;
-var obj = {
-  name: "Jonas",
-  city: "Lisbon"
-};
+// var age = 27;
+// var obj = {
+//   name: "Jonas",
+//   city: "Lisbon"
+// };
 
-function change(a, b) {
-  a = 30;
-  b.city = "San Francisco";
+// function change(a, b) {
+//   a = 30;
+//   b.city = "San Francisco";
+// }
+
+// change(age, obj);
+// console.log(age , obj.city); 
+// when we pass a primitive into a function a simple copy is created, so if we change a the variable won't be affected on the outside because it is a primitive. The reference of the object that we pass, will change since this is a copy.
+
+
+
+
+/////////////
+// Passing functions as arguments
+// Example 1:
+// var years = [1990, 1965, 1937, 2005, 1998];
+// function arrayCalc(arr, fn) {
+//   var arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+//   return arrRes;
+// }
+
+// function calculateAge(el) {
+//   return 2016 - el;
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+// console.log(ages);
+
+
+// Example 2:
+// var years = [1940, 1980];
+// function arrayCalc(arr, fn) {
+//   arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+//   return arrRes;
+// }
+
+// function calculateAge(el) {
+//   return 2016 - el;
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+// console.log(ages);
+
+
+// Example 3:
+// var years = [1990, 1965, 1937, 2005, 1998];
+// function arrayCalc(arr, fn) {
+//   var arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+//   return arrRes;
+// }
+
+// function calculateAge(el) {
+//   return 2016 - el;
+// }
+
+// function isFullAge(el) {
+//   return el >= 18;
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+// var fullAges = arrayCalc(ages, isFullAge);
+// console.log(ages);
+// console.log(fullAges);
+
+
+
+// Example 4:
+var years = [1990, 1965, 1937, 2005, 1998];
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
 }
 
-change(age, obj);
-console.log(age , obj.city); 
-// when we pass a primitive into a function a simple copy is created, so if we change a the variable won't be affected on the outside because it is a primitive. The reference of the object that we pass, will change since this is a copy.
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - (0.67 * el));
+  } else {
+    return -1;
+  }
+}
+
+var ages = arrayCalc(years, calculateAge); // calculateAge is a callback function cause we are not calling it. we want the arrayCalc function to later call the calculateAge function
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
